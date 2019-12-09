9c7cf9d224c74cf76e01803e8673576655ad6494
thebells1111 <thebells1111@gmail.com>
2019-12-09 12:49:15 -0600

    working on dateInterval

--- src/Components/Interval.svelte
+++ src/Components/Interval.svelte
@@ -1,12 +1,60 @@
 <script>
   import { currentProgram } from '../stores.js';
   import Datepicker from '../Components/Calendar/Datepicker.svelte';
-  let interval = 1;
+
+  let interval = Math.floor($currentProgram.dateInterval / 86400000);
+
+  function changeInterval(e) {
+    let newInterval = interval;
+
+    if (!Number(interval)) {
+      newInterval = '';
+    } else if (interval > 365) {
+      newInterval = Number(
+        interval
+          .toString()
+          .split('')
+          .pop()
+      );
+    } else {
+      newInterval = interval;
+    }
+
+    if (newInterval === 0) {
+      newInterval = '';
+    }
+
+    interval = newInterval;
+  }
+
+  function saveInterval() {
+    if (!interval) {
+      interval = 1;
+    }
+
+    currentProgram.selectDateInterval(interval);
+  }
+
+  function mouseScroll(e) {
+    if (e.deltaY > 0 && e.target.value === '1') {
+      interval = 366;
+    }
+    if (e.deltaY < 0 && e.target.value === '365') {
+      interval = 0;
+    }
+  }
 </script>
 
 <span>
   Run every
-  <input type="number" value={interval} />
+  <input
+    type="number"
+    bind:value={interval}
+    on:input={changeInterval}
+    on:blur={saveInterval}
+    on:wheel={mouseScroll}
+    on:keypress={e => (e.key === 'Enter' ? e.target.blur() : undefined)}
+  />
   day(s)
 </span>
 
--- src/stores.js
+++ src/stores.js
@@ -1,33 +1,33 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 const blankProgram = {
-  selectedStations: ["s1"],
+  selectedStations: ['s1'],
   dailyStart: 21600000,
   dailyStop: 64800000,
   dateStart: new Date(new Date().toLocaleDateString()).getTime(),
-  dateInterval: 86400000,
+  dateInterval: 172800000,
   timerOn: 0,
   timerOff: 0,
   timerDuration: 0,
   timerInterval: 0,
-  type: "interval",
-  DOW: []
+  type: 'interval',
+  DOW: [],
 };
 
 const initialStationNames = {
-  s1: "s1",
-  s2: "s2",
-  s3: "s3",
-  s4: "s4",
-  s5: "s5",
-  s6: "s6",
-  s7: "s7",
-  s8: "s8"
+  s1: 's1',
+  s2: 's2',
+  s3: 's3',
+  s4: 's4',
+  s5: 's5',
+  s6: 's6',
+  s7: 's7',
+  s8: 's8',
 };
 
 const initialState = {
   programs: [blankProgram],
-  programIndex: 0
+  programIndex: 0,
 };
 
 function _stationNames() {
@@ -38,7 +38,7 @@ function _stationNames() {
   );
 
   return {
-    subscribe
+    subscribe,
   };
 }
 
@@ -64,7 +64,12 @@ function _currentProgram() {
         p.type = data;
         return p;
       }),
-    reset: () => set({ n: 0 })
+    selectDateInterval: data =>
+      update(p => {
+        p.dateInterval = data;
+        return p;
+      }),
+    reset: () => set({ n: 0 }),
   };
 }
 
